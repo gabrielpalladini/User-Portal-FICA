782da0e3489ac1751242c1bfba1d966f19ac9d2b
gabrielpalladini <gmpalladini@gmail.com>
2021-03-07 21:24:05 +0100

    auth on going

--- dist/types.js
+++ dist/types.js
@@ -1,3 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+ > ;
+req: Request & { session: Express.Session };
+res: Response;
+;
 //# sourceMappingURL=types.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from 'type-graphql';
 import {HelloResolver} from "./resolvers/hello";
 import {PostResolver} from "./resolvers/post";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis'
+import {__prod__} from "./constants";
+import {MyContext} from "./types";
 
 const port = 5000;
 
@@ -13,19 +18,36 @@ const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    //const post = orm.em.create(Post, {title: 'my first title'});
-    //wait orm.em.persistAndFlush(post);
-    //const posts = await orm.em.find(Post, {});
-    //      console.log(posts);
-
     const app = express();
+
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                httpOnly: true,
+                sameSite: "lax", // csrf
+                secure: __prod__, //cookie only works in https
+
+            },
+            secret: 'asdihafuisohsigfdhihisdhfisd',
+            resave: false,
+        })
+    )
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em })
+        context: ({req, res }): MyContext => ({ em: orm.em, req, res }),
     });
 
     apolloServer.applyMiddleware({ app });
