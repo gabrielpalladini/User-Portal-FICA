253c11b9a8b9b3ff21a22c3cc6e01ecd05cf2af8
gabrielpalladini <gmpalladini@gmail.com>
2021-03-12 22:07:37 +0100

    begining of frontend

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface registerProps {
+};
+
+const Register: React.FC<registerProps> = ({}) => {
+  return <div> register page</div>;
+};
+
+export default Register;
