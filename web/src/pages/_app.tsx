f7ec62db3d3dcf1f7690a65a92358bbd0be138b8
gabrielpalladini <gmpalladini@gmail.com>
2021-03-29 23:16:01 +0200

    on going

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import {ThemeProvider, CSSReset} from '@chakra-ui/react'
+import { ThemeProvider, CSSReset } from '@chakra-ui/react'
 import theme from '../theme'
-import { Provider, createClient } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange, Cache, QueryInput} from '@urql/exchange-graphcache';
+import { MeDocument } from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any)
+}
 
 const client = createClient({
   url: "http://localhost:5001/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+          cache.updateQuery({query: MeDocument}, )
+        }
+      }
+    }
+  }), fetchExchange],
 });
 
 function MyApp({Component, pageProps}: any) {
