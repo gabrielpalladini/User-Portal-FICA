e221c3adf2a4bae304dcf534a7c9e55d8a66e4ae
gabrielpalladini <gmpalladini@gmail.com>
2021-04-18 21:17:59 +0200

    on going

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -36,8 +36,62 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => UserResponse)
+  async changePassword(
+    @Arg('token') token: string,
+    @Arg ('newPassword') newPassword: string,
+    @Ctx() {redis, em, req}: MyContext
+  ): Promise<UserResponse> {
+    if (newPassword.length <= 2) {
+      return {
+        errors: [
+        {
+          field: "newPassword",
+          message: "length must be greater than 2"
+        },
+      ],
+      };
+    }
+
+    const userId = await redis.get(FORGET_PASSWORD_PREFIX+token);
+    if (!userId) {
+      return { errors: [
+          {
+            field: "token",
+            message: "token expired"
+          },
+        ],
+      };
+    }
+
+    const user = await em.findOne(User, { id: parseInt(userId) });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "newPassword",
+            message: "length must be greater than 2"
+          },
+        ],
+      }
+    }
+
+    user.password = await argon2.hash(newPassword);
+    await em.persistAndFlush(user);
+
+    //log in user after changing password
+    req.session.userId = user.id;
+
+    return { user };
+
+  }
+
   @Mutation(() => Boolean)
-  async forgotPassword(@Arg("email") email: string, @Ctx() {em, redis}: MyContext) {
+  async forgotPassword(
+    @Arg("email") email: string,
+    @Ctx() { em, redis }: MyContext
+  ) {
     const user = await em.findOne(User, {email});
     if (!user) {
       //the email is not in the database
--- web/src/pages/change-password/[token].tsx
+++ web/src/pages/change-password/[token].tsx
@@ -1,8 +1,49 @@
 import {NextPage} from 'next';
+import {Form, Formik} from 'formik';
+import {toErrorMap} from '../../utils/toErrorMap';
+import {InputField} from '../../components/InputField';
+import {Box, Button} from '@chakra-ui/react';
+import {Wrapper} from '../../components/Wrapper';
+import * as React from 'react';
 
 
 const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
-  return <div>token is: {token}</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          // const response = await login(values);
+          // if (response.data?.login.errors) {
+          //   setErrors(toErrorMap(response.data.login.errors));
+          // } else if (response.data?.login.user) {
+          //   //worked
+          //   router.push('/')
+          // }
+        }}
+      >
+        {({isSubmitting}) => (
+          <Form>
+            <InputField
+              name='newPassword'
+              placeholder='new password'
+              label='New Password'
+              type='password'
+            />
+            <Button
+              mt={4}
+              type='submit'
+              colorScheme={'green'}
+              isLoading={isSubmitting}
+            >
+              change password
+            </Button>
+          </Form>
+        )}
+
+      </Formik>
+    </Wrapper>
+  );
 };
 
 ChangePassword.getInitialProps = ({ query}) => {
